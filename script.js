3bb0158fb02dd67c53ba55aedc6c3d85a20b79c4
Emma Beaton <emma.beaton@gmail.com>
2015-10-14 15:10:44 -0500

    made fixes to nav bar and margins

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ songs.push("Sir Duke by Stevie Wonder on the album Songs in the Key of Life");
 console.log(songs);
 
 var targetHTML = document.getElementById("song-container");
-targetHTML.innerHTML = "";
+var output = "";
 
 for (var i = 0; i < songs.length; i++) {
 	songs[i] = songs[i].replace(">", "");
@@ -20,8 +20,21 @@ for (var i = 0; i < songs.length; i++) {
 	songs[i] = songs[i].replace("(", "");
 	songs[i] = songs[i].replace("!", "");
 	songs[i] = songs[i].replace("on the album", "-");
-	targetHTML.innerHTML += "<p>" + songs[i] + "</p>";
+
+	var dashIndex = songs[i].indexOf("-");
+	console.log(dashIndex);
+
+	var songName = songs[i].slice(0, dashIndex);
+	console.log(songName);
+
+	var artistAlbumName = songs[i].slice(dashIndex, songs[i].length);
+	console.log(artistAlbumName);
+	output += "<div>";
+	output += "<h2>" + songName + "</h2>";
+	output += "<p>" + artistAlbumName + "</p>";
+	output += "</div>";
 }
+targetHTML.innerHTML = output;
 console.log(targetHTML);
 
 //Loop over the array
