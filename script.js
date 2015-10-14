55f57e5667c3ccbba0a5d21b3646035bcacbb1fc
Emma Beaton <emma.beaton@gmail.com>
2015-10-14 12:04:25 -0500

    started flex styling for nav bar

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-/*
-  1. Take the contents of the sonnet div and place it in a variable
-  2. Find and output the starting position of the word "orphans"
-  3. Output the number of characters in the sonnet
-  4. Replace the first occurance of the string "winter" with "yuletide"
-  5. Replace all occurances of the string "the" with "a large"
-  6. Set the content of the sonnet div with the new string
-*/
+var songs = [];
 
-var sonnetElement = document.getElementById("sonnet");
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+songs.unshift("Short People by Randy Newman on the album Little Criminals");
+songs.push("Sir Duke by Stevie Wonder on the album Songs in the Key of Life");
+console.log(songs);
 
-var sonnetText = sonnetElement.innerHTML;
-console.log("sonnetText", sonnetText);
+var targetHTML = document.getElementById("song-container");
+targetHTML.innerHTML = "";
 
-var indexOfOrphans = sonnetText.indexOf("orphans");
-console.log("indexOfOrphans", indexOfOrphans);
-console.log("length of sonnet", sonnetText.length)
+for (var i = 0; i < songs.length; i++) {
+	songs[i] = songs[i].replace(">", "");
+	songs[i] = songs[i].replace("by", "-");
+	songs[i] = songs[i].replace("*", "");
+	songs[i] = songs[i].replace("@", "");
+	songs[i] = songs[i].replace("(", "");
+	songs[i] = songs[i].replace("!", "");
+	songs[i] = songs[i].replace("on the album", "-");
+	targetHTML.innerHTML += "<p>";
+	targetHTML.innerHTML += songs[i];
+	targetHTML.innerHTML += "</p>";
+}
+console.log(songs);
 
-sonnetElement.innerHTML = sonnetText.replace("winter", "yuletide");
-sonnetElement.innerHTML = sonnetText.replace(/the/g, "a large");
+//Loop over the array
+//remove words and characters that don't belong
+//replace > with -
+
+
+
+
+
